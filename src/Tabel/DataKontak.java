e16c5087aa077108c1495e4c32cfe4773e824748
JonathanHalim54 <edgex54@gmail.com>
2017-02-06 18:09:54 +0700

    commit

---
+++ src/Tabel/DataKontak.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tabel;
+
+/**
+ *
+ * @author jonathan
+ */
+public class DataKontak {
+private String nama, telepon, e_mail; 
+
+public DataKontak(){   
+} 
+
+public String getE_mail() {
+return e_mail;
+}
+
+public void setE_mail(String e_mail) {
+this.e_mail = e_mail;
+}
+
+public String getNama() {
+return nama;
+}
+
+public void setNama(String nama) {
+this.nama = nama;
+}
+
+public String getTelepon() {
+return telepon;
+}
+
+public void setTelepon(String telepon) {
+this.telepon = telepon;
+}
+}
